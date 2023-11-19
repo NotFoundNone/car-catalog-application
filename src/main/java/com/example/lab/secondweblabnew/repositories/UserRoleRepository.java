456245217c1053e241e5fbc6d1f33ca2aca78da0
NotFoundNone1 <nd.dotcode@gmail.com>
2023-11-19 18:42:45 +0300

    update Repository (add annotation)

--- src/main/java/com/example/lab/secondweblabnew/repositories/UserRoleRepository.java
+++ src/main/java/com/example/lab/secondweblabnew/repositories/UserRoleRepository.java
@@ -1,6 +1,8 @@
 package com.example.lab.secondweblabnew.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository {
 }
