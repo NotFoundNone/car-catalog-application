eac555aa92f4b5fe482554a9f785e4b9a39a78dd
NotFoundNone1 <nd.dotcode@gmail.com>
2023-11-19 18:31:37 +0300

    add Repositories

---
+++ src/main/java/com/example/lab/secondweblabnew/repositories/ModelRepository.java
@@ -0,0 +1,9 @@
+package com.example.lab.secondweblabnew.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepository extends JpaRepository
+{
+}
