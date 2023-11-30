05a72ed86ccb631c0b1aff6a2fb484f1bcd074c3
NewUser <nd.dotcode@gmail.com>
2023-11-30 12:56:08 +0300

    update repos

--- src/main/java/com/example/lab/secondweblabnew/repositories/UserRepository.java
+++ src/main/java/com/example/lab/secondweblabnew/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.lab.secondweblabnew.repositories;
 
+import com.example.lab.secondweblabnew.models.Brand;
 import com.example.lab.secondweblabnew.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository <User, String> {
+    Optional<Brand> findByUsername(String username);
 }
