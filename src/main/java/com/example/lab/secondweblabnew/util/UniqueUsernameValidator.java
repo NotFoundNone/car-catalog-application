c3d128782112b3881ea88eceb89c28bd37081948
NewUser <nd.dotcode@gmail.com>
2023-11-30 12:55:05 +0300

    add validators

---
+++ src/main/java/com/example/lab/secondweblabnew/util/UniqueUsernameValidator.java
@@ -0,0 +1,19 @@
+package com.example.lab.secondweblabnew.util;
+
+import com.example.lab.secondweblabnew.repositories.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String>{
+
+    private final UserRepository userRepository;
+
+    public UniqueUsernameValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return userRepository.findByUsername(s).isEmpty();
+    }
+}
